e10d394f82d5da56261fddc1252e6677783c9f5f
dolprat <dolpratxyz@gmail.com>
2024-04-19 18:22:04 +0700

    add lorem

--- components/home.tsx
+++ components/home.tsx
@@ -6,7 +6,13 @@ function Home() {
   return (
     <div className="w-1/2">
       <Image src={Near} alt="picture of near" />
-      <div></div>
+      <div>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam saepe in
+        at architecto officiis blanditiis, non eos aliquid, sapiente
+        voluptatibus impedit sequi soluta optio nemo ut quae repellat est
+        consectetur.
+      </div>
     </div>
   );
 }
