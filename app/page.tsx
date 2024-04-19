1c598e189f197ceb83681d66ea3bbd0c20eefc7d
dolprat <dolpratxyz@gmail.com>
2024-04-19 18:10:04 +0700

    add picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
 import Nav from "../components/navbar";
-import Test from "../components/test";
+import Home from "../components/home";
 
 export default function page() {
   return (
     <>
       <Nav />
+      <Home />
     </>
   );
 }
---
+++ components/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Near from "../public/200iq-near.gif";
+import Image from "next/image";
+
+function Home() {
+  return (
+    <div className="w-1/2">
+      <Image src={Near} />
+      <div></div>
+    </div>
+  );
+}
+
+export default Home;
